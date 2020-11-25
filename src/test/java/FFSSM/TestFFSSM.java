6e19f6ad9ea7cb9f212800b53fd308caa3d09325
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-25 12:39:29 +0100

    Implémentation classe Test

--- src/test/java/FFSSM/TestFFSSM.java
+++ src/test/java/FFSSM/TestFFSSM.java
@@ -1,7 +1,10 @@
 package FFSSM;
 
 import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 
@@ -20,11 +23,31 @@ public class TestFFSSM {
     Moniteur organisateur;
     Plongeur untel;
     Plongee palanquee;
-    LocalDate date;
+    LocalDate dateDDNOrg,dateDDNPlo,datePlongee,dateDelivrancePerimee,dateDelivranceValable;
+    Site site;
+    Licence l,l2;
     
     @BeforeEach
+    public void setUp() throws Exception{
+        dateDDNOrg = LocalDate.of(1980, 1, 1);
+        dateDDNPlo = LocalDate.of(1975, 2, 2);
+        datePlongee = LocalDate.of(2020, 11, 25);
+        dateDelivrancePerimee = LocalDate.of(2018, 1, 1);
+        dateDelivranceValable = LocalDate.of(2020, 1, 1);
+        site = new Site("Méditérannée","Initiation aux SSM");
+        organisateur = new Moniteur("0001","Dupont","Jean","1 Rue de la paix","0606060606",dateDDNOrg,1);
+        club = new Club(organisateur,"Club de Marseille de SSM","0505050505","1 Rue de la plongée");
+        untel = new Plongeur(0,"1111","Leblanc","Patrick","2 Rue de la Volonté","0606060607",dateDDNPlo,GroupeSanguin.APLUS);
+        palanquee = new Plongee(site,organisateur,datePlongee, 10,2);
+        l = new Licence(untel,"001",dateDelivrancePerimee,0,club);
+        l2 = new Licence(untel,"002",dateDelivranceValable,0,club);
+    }
     
-    
+    @Test
+    public void testValiditéLicence(){
+        assertFalse(l.estValide(datePlongee),"La licence devrait être périmée");
+        assertTrue(l2.estValide(datePlongee),"La licence ne devrait pas être périmée");        
+    }
     //Validité de la licence à tester
     // Embauche terminée
 }
