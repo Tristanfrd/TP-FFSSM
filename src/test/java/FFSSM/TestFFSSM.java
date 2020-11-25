1c9e7a5cd92ee4ec67beb0c3fd62ae64d398fc6b
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-25 11:46:21 +0100

    commit général, création de la classe de test

---
+++ src/test/java/FFSSM/TestFFSSM.java
@@ -0,0 +1,30 @@
+package FFSSM;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author frede
+ */
+public class TestFFSSM {
+    Club club;
+    Moniteur organisateur;
+    Plongeur untel;
+    Plongee palanquee;
+    LocalDate date;
+    
+    @BeforeEach
+    
+    
+    //Validité de la licence à tester
+    // Embauche terminée
+}
