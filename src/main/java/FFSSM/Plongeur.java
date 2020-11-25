ee8f1e88cfb43fdf4d4a0bb54ba4910baf2b4522
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-25 11:05:42 +0100

    Plongeur

--- src/main/java/FFSSM/Plongeur.java
+++ src/main/java/FFSSM/Plongeur.java
@@ -18,5 +18,43 @@ public class Plongeur extends Personne{
         Licence l = new Licence(this,numéro,delivrance,this.niveau,club);
         licence.add(l);
     }
+
+    public GroupeSanguin getGS() {
+        return GS;
+    }
+
+    public void setGS(GroupeSanguin GS) {
+        this.GS = GS;
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(int niveau) {
+        this.niveau = niveau;
+    }
+
+    public ArrayList<Licence> getLicence() {
+        return licence;
+    }
+
+    public void setLicence(ArrayList<Licence> licence) {
+        this.licence = licence;
+    }
+    
+    public Licence getDernierelicence(){
+        Licence l = licence.get(licence.size()-1);
+        return l;
+}
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
+    
         
 }
