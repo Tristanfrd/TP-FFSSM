870de1cf2e55e67c8c41370a19021c4b0e2f9507
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-25 10:42:31 +0100

    Plongeur

--- src/main/java/FFSSM/Plongeur.java
+++ src/main/java/FFSSM/Plongeur.java
@@ -1,5 +1,22 @@
 package FFSSM;
 
-public class Plongeur {
-	
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Plongeur extends Personne{
+        private GroupeSanguin GS;
+	private int niveau;
+        private ArrayList<Licence> licence = new ArrayList <>();
+        private Club club;
+        
+    public Plongeur(int niveau, String numeroINSEE, String nom, String prenom, String adresse, String telephone, LocalDate naissance, GroupeSanguin GS) {
+        super(numeroINSEE, nom, prenom, adresse, telephone, naissance);
+        this.niveau = niveau;
+        this.GS = GS;
+    }
+    public void ajouterLicence(String numéro, LocalDate delivrance){
+        Licence l = new Licence(this,numéro,delivrance,this.niveau,club);
+        licence.add(l);
+    }
+        
 }
