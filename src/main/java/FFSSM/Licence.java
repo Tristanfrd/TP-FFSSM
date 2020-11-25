185a8341461fc343a1a653adc5c54eb33735349e
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-25 09:19:22 +0100

    Licence fini

--- src/main/java/FFSSM/Licence.java
+++ src/main/java/FFSSM/Licence.java
@@ -16,6 +16,8 @@ public class Licence {
     public int niveau;
 
     public Club club;
+    
+    private boolean validité;
 
     public Licence(Personne possesseur, String numero, LocalDate delivrance, int niveau, Club club) {
         this.possesseur = possesseur;
@@ -52,8 +54,12 @@ public class Licence {
      * @return vrai si valide à la date d
      **/
     public boolean estValide(LocalDate d) {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+         if (this.delivrance.compareTo(d)>=0){
+             validité = true;
+         }else{
+             validité = false;
+         }
+         return validité;
     }
 
 }
