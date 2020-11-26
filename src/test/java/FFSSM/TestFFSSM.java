52ccca2abf21f4b4e0ff4b1e302d2a9e20918327
Tristanfrd <Tristanfrd@users.noreply.github.com>
2020-11-26 11:41:44 +0100

    Fin

--- src/test/java/FFSSM/TestFFSSM.java
+++ src/test/java/FFSSM/TestFFSSM.java
@@ -1,6 +1,7 @@
 package FFSSM;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,9 +22,9 @@ import org.junit.jupiter.api.Test;
 public class TestFFSSM {
     Club club;
     Moniteur organisateur;
-    Plongeur untel;
-    Plongee palanquee;
-    LocalDate dateDDNOrg,dateDDNPlo,datePlongee,dateDelivrancePerimee,dateDelivranceValable;
+    Plongeur untel,untel2;
+    Plongee palanquee,palanquee2;
+    LocalDate dateDDNOrg,dateDDNPlo,datePlongee,dateDelivrancePerimee,dateDelivranceValable,debutEmbauche,finEmbauche;
     Site site;
     Licence l,l2;
     
@@ -34,11 +35,16 @@ public class TestFFSSM {
         datePlongee = LocalDate.of(2020, 11, 25);
         dateDelivrancePerimee = LocalDate.of(2018, 1, 1);
         dateDelivranceValable = LocalDate.of(2020, 1, 1);
+        debutEmbauche = LocalDate.of(2019, 01, 01);
+        finEmbauche = LocalDate.of(2020, 01,01);
         site = new Site("Méditérannée","Initiation aux SSM");
         organisateur = new Moniteur("0001","Dupont","Jean","1 Rue de la paix","0606060606",dateDDNOrg,1);
         club = new Club(organisateur,"Club de Marseille de SSM","0505050505","1 Rue de la plongée");
         untel = new Plongeur(0,"1111","Leblanc","Patrick","2 Rue de la Volonté","0606060607",dateDDNPlo,GroupeSanguin.APLUS);
+        untel2= new Plongeur(0,"2222","Legris","Enzo","4 Rue de la Volonté","0606060707",dateDDNPlo,GroupeSanguin.AMOINS);
         palanquee = new Plongee(site,organisateur,datePlongee, 10,2);
+        palanquee2 = new Plongee(site,organisateur,datePlongee, 10,2);
+
         l = new Licence(untel,"001",dateDelivrancePerimee,0,club);
         l2 = new Licence(untel,"002",dateDelivranceValable,0,club);
     }
@@ -48,6 +54,35 @@ public class TestFFSSM {
         assertFalse(l.estValide(datePlongee),"La licence devrait être périmée");
         assertTrue(l2.estValide(datePlongee),"La licence ne devrait pas être périmée");        
     }
-    //Validité de la licence à tester
-    // Embauche terminée
+    
+    @Test
+    public void testPlongeesNonConformes(){
+        untel.ajouterLicence("001", dateDelivrancePerimee);
+        palanquee.ajouteParticipant(untel);
+        club.organisePlongee(palanquee);
+        assertTrue(club.plongeesNonConformes().contains(palanquee),"La plongée doit être non conforme car le participant a une licence périmée.");
+        untel2.ajouterLicence("002", dateDelivranceValable);
+        palanquee2.ajouteParticipant(untel2);
+        club.organisePlongee(palanquee2);
+        assertFalse(club.plongeesNonConformes().contains(palanquee2),"La plongée ne doit pas apparaître dans la collection.");
+    }
+    
+    @Test
+    public void testEmployeurActuel(){
+        organisateur.nouvelleEmbauche(club, debutEmbauche);
+        assertTrue(organisateur.employeurActuel().equals(Optional.ofNullable(club)),"Le club devrait être l'employeur.");
+        organisateur.terminerEmbauche(finEmbauche);
+        assertTrue(organisateur.employeurActuel().equals(Optional.empty()),"L'employeur devrait être vide.");
+    }
+    
+    @Test
+    public void testEstConforme(){
+        untel.ajouterLicence("003", dateDelivrancePerimee);
+        untel2.ajouterLicence("004",dateDelivranceValable);
+        palanquee.ajouteParticipant(untel2);
+        assertTrue(palanquee.estConforme(),"La palanquée devrait être conforme.");
+        palanquee.ajouteParticipant(untel);
+        assertFalse(palanquee.estConforme(),"La palanquée ne devrait pas être conforme.");
+    }
+
 }
